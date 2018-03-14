9652679ff6bcd1e78d04f15929970a7a5087cc7d
Kelong Wang <buaawkl@gmail.com>
2018-03-14 00:31:07 +0000

    Run setup outside the folder

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+if [ ! -d ~/.vim ]; then
+	echo "Please clone to ~/.vim"
+	exit 1
+fi
+
+cd ~/.vim
 # fetch submodule content
 git submodule init
 git submodule update
